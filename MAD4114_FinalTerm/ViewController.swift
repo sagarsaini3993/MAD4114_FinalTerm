05659ac5d421ab5c7c28c90cb282e5f531f60f8f
MacStudent <macstudent@128-imac22.local>
2018-11-22 08:41:52 -0500

    Initial Commit

---
+++ MAD4114_FinalTerm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAD4114_FinalTerm
+//
+//  Created by MacStudent on 2018-11-22.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
